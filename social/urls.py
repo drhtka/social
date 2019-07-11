89483d75eb238faa3b0d4c284160c8ce2d59a486
ubuntu <drhtka@gmail.com>
2019-07-11 22:35:57 +0300

    setup_wysiwyg

--- social/urls.py
+++ social/urls.py
@@ -18,8 +18,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include, re_path
 from index import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^account/', include("account.urls")),
     re_path('^$', views.index),
-]
+    path('summernote/', include('django_summernote.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social_content/admin.py
+++ social_content/admin.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from social_content.models import Content
 
-# Register your models here.
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('entry',)
+
+admin.site.register(Content, NewsAdmin)
